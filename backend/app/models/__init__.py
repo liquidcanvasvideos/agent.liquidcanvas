275889ae514202e09576303b413b2d89c27a3255
Crys Garage Developer <developer@crysgarage.com>
2025-11-29 16:17:53 +0100

    Import Settings model in models __init__

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,5 +4,6 @@ Import all models here for Alembic to detect them
 from app.models.prospect import Prospect
 from app.models.job import Job
 from app.models.email_log import EmailLog
+from app.models.settings import Settings
 
 __all__ = ["Prospect", "Job", "EmailLog"]
